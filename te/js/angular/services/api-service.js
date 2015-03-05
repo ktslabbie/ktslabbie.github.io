67f04d1b99aedb1100965525634c187d47e7a228
Kristian <ktslabbie@gmail.com>
2015-03-05 20:31:34 +0900

    Changed the API address to the remote AWS server.

--- te/js/angular/services/api-service.js
+++ te/js/angular/services/api-service.js
@@ -1,25 +1,26 @@
 var twitterAPIService = angular.module('twitterWeb.APIService', ['ngResource']);
+var apiHostName = "http://ec2-54-65-213-114.ap-northeast-1.compute.amazonaws.com:8090/api/"
 
 twitterAPIService.factory('SimpleUser', ['$resource', function($resource) {
-	return $resource('/api/get-simple-user', { }, {
+	return $resource(apiHostName + 'get-simple-user', { }, {
 						get: { isArray: false, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('User', ['$resource', function($resource) {
-	return $resource('/api/get-user', { }, {
+	return $resource(apiHostName + 'get-user', { }, {
 						get: { isArray: false, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('FollowersList', ['$resource', function($resource) {
-	return $resource('/api/get-followers-list', { }, {
+	return $resource(apiHostName + 'get-followers-list', { }, {
 						list: { isArray: true, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('KeywordUserList', ['$resource', function($resource) {
-	return $resource('/api/get-keyword-user-list', { }, {
+	return $resource(apiHostName + 'get-keyword-user-list', { }, {
 						list: { isArray: true, method: 'get'}
 	});
 }]);
