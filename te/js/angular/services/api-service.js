9ac2511dec8cfeea874c7628f3e405834460a61d
Kristian Slabbekoorn <ktslabbie@gmail.com>
2015-04-07 16:13:37 +0900

    Update api-service.js

--- te/js/angular/services/api-service.js
+++ te/js/angular/services/api-service.js
@@ -1,25 +1,26 @@
 var twitterAPIService = angular.module('twitterWeb.APIService', ['ngResource']);
+var apiHostName = "http://" + SETTINGS.addrOne + "." + SETTINGS.addrTwo + ":" + SETTINGS.prt + SETTINGS.path;
 
 twitterAPIService.factory('SimpleUser', ['$resource', function($resource) {
-	return $resource(SETTINGS.apiHostName + 'get-simple-user', { }, {
+	return $resource(apiHostName + 'get-simple-user', { }, {
 						get: { isArray: false, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('User', ['$resource', function($resource) {
-	return $resource(SETTINGS.apiHostName + 'get-user', { }, {
+	return $resource(apiHostName + 'get-user', { }, {
 						get: { isArray: false, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('FollowersList', ['$resource', function($resource) {
-	return $resource(SETTINGS.apiHostName + 'get-followers-list', { }, {
+	return $resource(apiHostName + 'get-followers-list', { }, {
 						list: { isArray: true, method: 'get'}
 	});
 }]);
 
 twitterAPIService.factory('KeywordUserList', ['$resource', function($resource) {
-	return $resource(SETTINGS.apiHostName + 'get-keyword-user-list', { }, {
+	return $resource(apiHostName + 'get-keyword-user-list', { }, {
 						list: { isArray: true, method: 'get'}
 	});
 }]);
